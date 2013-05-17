4cad53a6966fee64d62077e005daebb2d314b113
olcay tarazan <olcaytarazan@gmail.com>
2013-05-18 00:02:40 +0200

    user registration is added

---
+++ workspace/enwida-web/src/main/java/de/enwida/web/UserManagement.java
@@ -0,0 +1,77 @@
+package de.enwida.web;
+
+import java.security.Principal;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import de.enwida.dao.User;
+/**
+ * Handles requests for the application registration pages.
+ */
+@Controller
+public class UserManagement {
+	
+	@RequestMapping(value="/welcome", method = RequestMethod.GET)
+	public String printWelcome(ModelMap model, Principal principal ) {
+		String name="unknown";
+		if(principal!=null){
+		name = principal.getName();
+		}
+		model.addAttribute("username", name);
+		return "home";
+ 
+	}
+ 
+	@RequestMapping(value="/login", method = RequestMethod.GET)
+	public String login(ModelMap model) {
+ 
+		return "login";
+ 
+	}
+	
+	@RequestMapping(value="/404", method = RequestMethod.GET)
+	public String error404(ModelMap model) {
+ 
+		return "404";
+ 
+	}
+ 
+	@RequestMapping(value="/loginfailed", method = RequestMethod.GET)
+	public String loginerror(ModelMap model) {
+ 
+		model.addAttribute("error", "true");
+		return "login";
+ 
+	}
+ 
+	@RequestMapping(value="/logout", method = RequestMethod.GET)
+	public String logout(ModelMap model) {
+ 
+		return "logout";
+ 
+	}
+	
+	@RequestMapping(value="/register",method=RequestMethod.GET)
+    public String showForm(ModelMap model){
+        User user = new User();
+        model.addAttribute("USER", user);
+        return "register";
+    }
+
+    @RequestMapping(value="/register",method=RequestMethod.POST)
+    public String processForm(@ModelAttribute(value="USER") User user,BindingResult result){
+        if(result.hasErrors()){
+            return "registration";
+        }else{
+            System.out.println("User values is : " + user);
+            return "hello";
+        }
+    }
+}
