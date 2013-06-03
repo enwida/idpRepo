9910827b6d2841f8145d3a27ad2607a955733952
Daniel Strittmatter <daniel@smattr.de>
2013-06-03 13:46:18 +0200

    Add skeleton implementation of navigation service

--- workspace/enwida/src/main/java/de/enwida/web/service/implementation/NavigationServiceImpl.java
+++ workspace/enwida/src/main/java/de/enwida/web/service/implementation/NavigationServiceImpl.java
@@ -3,31 +3,32 @@
  */
 package de.enwida.web.service.implementation;
 
+import java.util.Locale;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import de.enwida.chart.DataRequestManager;
 import de.enwida.web.dao.interfaces.INavigationDao;
-import de.enwida.web.model.NavigationDataStructure;
+import de.enwida.web.model.ChartNavigationData;
+import de.enwida.web.model.User;
 import de.enwida.web.service.interfaces.NavigationService;
 
-/**
- * @author Jitin
- *
- */
 @Service("NavigationService")
 @Transactional
 public class NavigationServiceImpl implements NavigationService {
 
 	@Autowired
 	private DataRequestManager dataRequestManager;
-
+	
 	@Autowired
 	private INavigationDao navigationDao;
 
-	public void getNavigationData(NavigationDataStructure navigationDS) {
-		// TODO : update navigation datastructure here
-	}
+    public ChartNavigationData getNavigationData(int chartId, User user, Locale locale) {
+        ChartNavigationData navigationData = navigationDao.getDefaultNavigation(chartId, locale);
+        
+        return navigationData;
+    }
 
 }
