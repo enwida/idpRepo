e2253b59369b8537ba2f948fa81bc0d8f7d75a67
Daniel Strittmatter <daniel@smattr.de>
2013-06-03 15:22:07 +0200

    Add ProductRestriction class

---
+++ workspace/enwida/src/main/java/de/enwida/web/utils/ProductRestriction.java
@@ -0,0 +1,56 @@
+package de.enwida.web.utils;
+
+import java.util.List;
+
+import de.enwida.chart.Aspect;
+import de.enwida.transport.DataResolution;
+
+public class ProductRestriction {
+    
+    private int productId;
+    private Aspect aspect;
+    private List<DataResolution> resolutions;
+    private CalendarRange timeRange;
+
+    public ProductRestriction(int productId, Aspect aspect,
+           List<DataResolution> resolutions, CalendarRange timeRange) {
+
+        this.productId = productId;
+        this.aspect = aspect;
+        this.resolutions = resolutions;
+        this.timeRange = timeRange;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public Aspect getAspect() {
+        return aspect;
+    }
+
+    public void setAspect(Aspect aspect) {
+        this.aspect = aspect;
+    }
+
+    public List<DataResolution> getResolutions() {
+        return resolutions;
+    }
+
+    public void setResolutions(List<DataResolution> resolutions) {
+        this.resolutions = resolutions;
+    }
+
+    public CalendarRange getTimeRange() {
+        return timeRange;
+    }
+
+    public void setTimeRange(CalendarRange timeRange) {
+        this.timeRange = timeRange;
+    }
+    
+}
