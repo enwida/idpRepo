242b74b3ce5d65ed32956f77aa942dd3ce0e28a4
olcay <o.tarazan@tum.de>
2013-07-10 10:41:56 +0200

    LogoFinder updated to get specific image sizes( smaller than 200x200)

--- workspace/enwida/src/main/java/de/enwida/web/utils/LogoFinder.java
+++ workspace/enwida/src/main/java/de/enwida/web/utils/LogoFinder.java
@@ -1,6 +1,10 @@
 package de.enwida.web.utils;
 
+import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
 
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.DomElement;
@@ -12,7 +16,7 @@ public class LogoFinder {
     public String getImages(String companyName) {
         final WebClient webClient = new WebClient();
         HtmlPage page;
-        String companyLink="http://"+companyName+"/";
+        String companyLink="https://"+companyName+"/";
         String images="";
         try {
             page = webClient.getPage(companyLink);
@@ -22,13 +26,20 @@ public class LogoFinder {
             final DomNodeList<DomElement> div = page.getElementsByTagName("img");
             for (DomElement domElement : div) {
                 String imgSrc=domElement.asXml();
+                String url=null;
                 //if this is not an absolute path update it
                 if (!imgSrc.contains("http")){
                     imgSrc=imgSrc.replace("src=\"", "src=\""+companyLink);
                     imgSrc=imgSrc.replace("src='", "src='"+companyLink);
-                    
+                    url = companyLink+domElement.getAttribute("src"); 
+                }else{
+                    url = domElement.getAttribute("src"); 
                 }
-                images+=imgSrc;
+                url.replace("http", "https");
+                final BufferedImage bi = ImageIO.read(new URL(url));
+                if (bi != null && bi.getHeight() < 200 && bi.getWidth() < 200) {
+                    images += imgSrc;
+                }
          }
         } catch ( IOException e) {
             // TODO Auto-generated catch block
