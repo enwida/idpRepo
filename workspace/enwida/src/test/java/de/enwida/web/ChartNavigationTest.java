8f157aa2c11a243c334e9beb81eb40a8425cd394
Daniel Strittmatter <daniel@smattr.de>
2013-07-30 11:34:06 +0200

    Add tests for navigation service

---
+++ workspace/enwida/src/test/java/de/enwida/web/ChartNavigationTest.java
@@ -0,0 +1,146 @@
+package de.enwida.web;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Locale;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import de.enwida.web.model.ChartNavigationData;
+import de.enwida.web.model.ProductTree;
+import de.enwida.web.model.ProductTree.ProductAttributes;
+import de.enwida.web.service.interfaces.INavigationService;
+import de.enwida.web.utils.CalendarRange;
+import de.enwida.web.utils.NavigationDefaults;
+ 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:/root-context-test.xml")
+public class ChartNavigationTest {
+
+	@Autowired
+	private INavigationService navigationService;
+	
+	// TODO: Setup users
+	
+	@Test
+	public void serviceIsAvailable() {
+		Assert.assertNotNull(navigationService);
+	}
+	
+	@Test
+	public void returnsValidDefaultNavigations() {
+		for (final ChartNavigationData navigationData : navigationService.getAllDefaultNavigationData().values()) {
+			checkNavigationData(navigationData);
+		}
+	}
+	
+	@Test
+	public void defaultRestrictionsArentExtended() {
+		for (final Integer key : navigationService.getAllDefaultNavigationData().keySet()) {
+			final ChartNavigationData defaultNavigation = navigationService.getDefaultNavigationData(key);
+			// TODO user
+			final ChartNavigationData restrictedNavigation = navigationService.getNavigationData(key, null, Locale.ENGLISH);
+			
+			Assert.assertTrue(defaultNavigation.getProductTrees().size() >= restrictedNavigation.getProductTrees().size());
+			
+			for (final int tso : restrictedNavigation.getTsos().keySet()) {
+				// Restricted navigation data contains only TSOs which are in the default navigation data
+				Assert.assertTrue(defaultNavigation.getTsos().keySet().contains(tso));
+
+				// Find the corresponding tree in the default navigation data
+				ProductTree defaultTree = null;
+				for (final ProductTree tree : defaultNavigation.getProductTrees()) {
+					if (tree.getTso() == tso) {
+						defaultTree = tree;
+						break;
+					}
+				}
+				Assert.assertNotNull(defaultTree);
+
+				// Find the corresponding tree in the restricted navigation data
+				ProductTree restrictedTree = null;
+				for (final ProductTree tree : restrictedNavigation.getProductTrees()) {
+					if (tree.getTso() == tso) {
+						restrictedTree = tree;
+						break;
+					}
+				}
+				Assert.assertNotNull(restrictedTree);
+				
+				final List<ProductAttributes> defaultProducts = defaultTree.flatten();
+				final List<ProductAttributes> restrictedProducts = restrictedTree.flatten();
+
+				for (final ProductAttributes restrictedProduct : restrictedProducts) {
+					// Product has to exist in default navigation, too
+					ProductAttributes defaultProduct = null;
+					for (final ProductAttributes currentProduct : defaultProducts) {
+						if (currentProduct.productId == restrictedProduct.productId) {
+							defaultProduct = currentProduct;
+							break;
+						}
+					}
+					Assert.assertNotNull(defaultProduct);
+
+					// Restricted resolutions is a subset of default resolutions
+					Assert.assertTrue(defaultProduct.resolutions.containsAll(restrictedProduct.resolutions));
+					
+					// Default time range is not extended
+					Assert.assertTrue(restrictedProduct.timeRange.getFrom().compareTo(defaultProduct.timeRange.getFrom()) >= 0);
+					Assert.assertTrue(restrictedProduct.timeRange.getTo().compareTo(defaultProduct.timeRange.getTo()) <= 0);
+				}
+			}
+		}
+	}
+	
+	private void checkNavigationData(ChartNavigationData navigationData) {
+		Assert.assertNotNull(navigationData);
+		
+		Assert.assertNotNull(navigationData.getTitle());
+		Assert.assertNotNull(navigationData.getxAxisLabel());
+		Assert.assertNotNull(navigationData.getyAxisLabel());
+
+		Assert.assertNotNull(navigationData.getAllResolutions());
+		Assert.assertFalse(navigationData.getAllResolutions().isEmpty());
+		
+		Assert.assertNotNull(navigationData.getAspects());
+		Assert.assertFalse(navigationData.getAspects().isEmpty());
+		
+		Assert.assertNotNull(navigationData.getProductTrees());
+		Assert.assertFalse(navigationData.getProductTrees().isEmpty());
+		
+		checkNavigationDataDefaults(navigationData.getDefaults());
+		
+		// Only one tree for a specific TSO
+		final Set<Integer> tsos = new HashSet<>();
+		for (final ProductTree tree : navigationData.getProductTrees()) {
+			// Tree isn't empty
+			Assert.assertNotNull(tree.getRoot());
+			Assert.assertFalse(tree.getRoot().getChildren().isEmpty());
+			
+			// Assert the element wasn't in the set already
+			Assert.assertTrue(tsos.add(tree.getTso()));
+		}
+	}
+	
+	private void checkNavigationDataDefaults(NavigationDefaults defaults) {
+		Assert.assertNotNull(defaults);
+		Assert.assertNotNull(defaults.getDisabledLines());
+		checkCalendarRange(defaults.getTimeRange());
+	}
+	
+	private void checkCalendarRange(CalendarRange range) {
+		Assert.assertNotNull(range);
+		Assert.assertNotNull(range.getFrom());
+		Assert.assertNotNull(range.getTo());
+		
+		// No negative range allowed
+		Assert.assertTrue(range.getFrom().compareTo(range.getTo()) <= 0);
+	}
+	
+}
