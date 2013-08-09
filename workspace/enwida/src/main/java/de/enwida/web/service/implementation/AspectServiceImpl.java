95aa19516647669af8199c49259a193f8e8dc0cf
olcay <o.tarazan@tum.de>
2013-08-10 01:01:08 +0200

    Removing redundant AspectDaoImpl.java. it is replaced by RightsDaoImpl

--- workspace/enwida/src/main/java/de/enwida/web/service/implementation/AspectServiceImpl.java
+++ workspace/enwida/src/main/java/de/enwida/web/service/implementation/AspectServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 import de.enwida.web.dao.interfaces.IRightDao;
