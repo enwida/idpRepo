cc9abb545944597ec1b4643815bf238d1365bf1e
jitin kumar <jitinkumar.baghel@outlook.com>
2013-09-01 00:27:58 +0200

    Updated userlines in datamanager

---
+++ workspace/database/user_lines_ddl.sql
@@ -0,0 +1,17 @@
+﻿-- Table: public.user_lines
+
+DROP TABLE IF EXISTS public.user_lines;
+
+CREATE TABLE public.user_lines
+(
+  id serial NOT NULL,
+  "timestamp" timestamp with time zone,
+  value double precision,
+  metadata_id integer,
+  CONSTRAINT user_lines_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.user_lines
+  OWNER TO enwida;
