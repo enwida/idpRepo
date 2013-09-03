383b85a281d5a4fbc9b8c9da2ae1635c34ba3876
Daniel Strittmatter <daniel@smattr.de>
2013-09-04 00:10:34 +0200

    Add timezone info selection (issue #59)

--- workspace/enwida/src/main/java/de/enwida/web/utils/timestamp/ITimestampFormatter.java
+++ workspace/enwida/src/main/java/de/enwida/web/utils/timestamp/ITimestampFormatter.java
@@ -1,9 +1,12 @@
 package de.enwida.web.utils.timestamp;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public interface ITimestampFormatter {
 	
 	public String format(Date date);
+	public SimpleDateFormat getDateFormat();
+	public void setDateFormat(SimpleDateFormat format);
 
 }
--- workspace/enwida/src/main/java/de/enwida/web/utils/timestamp/UTCTimestampFormatter.java
+++ workspace/enwida/src/main/java/de/enwida/web/utils/timestamp/UTCTimestampFormatter.java
@@ -2,14 +2,24 @@ package de.enwida.web.utils.timestamp;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class UTCTimestampFormatter implements ITimestampFormatter {
 
-	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-
+	private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	
 	@Override
 	public String format(Date date) {
+		dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
 		return dateFormat.format(date);
+	}
+	
+	public SimpleDateFormat getDateFormat() {
+		return dateFormat;
+	}
+	
+	public void setDateFormat(SimpleDateFormat dateFormat) {
+		this.dateFormat = dateFormat;
 	}
 
 }
