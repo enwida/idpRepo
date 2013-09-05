b0e3f42a9b473aa2a846b4c216f4c578a418bc74
olcay <o.tarazan@tum.de>
2013-09-05 17:55:26 +0200

    Adding User Line Request test

--- workspace/enwida/src/test/java/de/enwida/web/service/UserLinesServiceTest.java
+++ workspace/enwida/src/test/java/de/enwida/web/service/UserLinesServiceTest.java
@@ -5,6 +5,9 @@ package de.enwida.web.service;
 
 import static org.junit.Assert.fail;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
@@ -12,9 +15,16 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import de.enwida.chart.LineManager;
+import de.enwida.transport.Aspect;
+import de.enwida.transport.DataResolution;
+import de.enwida.transport.LineRequest;
+import de.enwida.transport.XYDataLine;
 import de.enwida.web.service.interfaces.IUserLinesService;
 
 /**
@@ -25,11 +35,13 @@ import de.enwida.web.service.interfaces.IUserLinesService;
 @ContextConfiguration(locations = "classpath:/root-context-test.xml")
 public class UserLinesServiceTest {
 
-	@Autowired
-	private IUserLinesService userLineService;
+    @Autowired
+    private IUserLinesService userLineService;
+
+    @Autowired
+    private LineManager lineManager;
 
 	private Logger logger = Logger.getLogger(getClass());
-
 	/**
 	 * @throws java.lang.Exception
 	 */
@@ -106,5 +118,22 @@ public class UserLinesServiceTest {
 	public void testEraseUserLineMetaData() {
 		fail("Not yet implemented");
 	}
+
+    @Test
+    public void getUserLine() {
+        Calendar c1 = new GregorianCalendar();
+        c1.setTimeInMillis(0);
+        Calendar c2 = new GregorianCalendar();
+        c2.set(Calendar.YEAR, 2020);
+        final LineRequest request = new LineRequest(Aspect.UL_TH_LOAD_PROFILE, 
+                1, 0,  c1, c2, DataResolution.DAILY , null);
+        try {
+            XYDataLine x = lineManager.getLine(request);
+            System.out.print(x.getDataPoints().size());
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
 }
